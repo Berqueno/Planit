33fb738dea23a3e174172b82968b132b32356e48
Berkay Berqueno <retroanonim@gmail.com>
2025-07-14 00:38:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Planit',
+  base: '/Planit/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
